9eb387f5dd3e5dc91459b36b74cf10bc774cfa08
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2024-05-12 00:36:29 -0500

    Corrected wsgi.py settings_module

--- ArticleProject/wsgi.py
+++ ArticleProject/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'ArticleProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'AzureProject.settings'
+settings_module = 'ArticleProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ArticleProject.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
