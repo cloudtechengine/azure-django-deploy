f13854f9c657cbce32b239c6e1614f7c76d1d612
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2024-05-14 20:56:36 -0500

    modified the ROOT_URL folder

--- ArticleProject/settings.py
+++ ArticleProject/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Bootstrap Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = 'static/CSS'
 
 # STATICFILES_DIRS = [BASE_DIR, 'blog/static']
 
@@ -139,7 +139,10 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('blog/static')),)
 
-STATIC_ROOT = "/var/www/example.com/static/"
+# STATIC_ROOT = "/var/www/example.com/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+
 # STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 MEDIA_URL = '/media/'
