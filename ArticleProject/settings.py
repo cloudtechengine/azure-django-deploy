eeac32481298cd9073ef28b060ff04daa6fff5af
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2024-05-15 13:37:54 -0500

    made changes in deployment and settings.py

--- ArticleProject/settings.py
+++ ArticleProject/settings.py
@@ -19,9 +19,9 @@ from django.contrib import staticfiles
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
