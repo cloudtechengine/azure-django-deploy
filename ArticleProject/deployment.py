1cffa8fe98f4705f796a56f81fa4274c2b787dc6
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2024-05-15 13:25:07 -0500

    made changes in deployment and settings.py

--- ArticleProject/deployment.py
+++ ArticleProject/deployment.py
@@ -23,7 +23,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = "/var/www/example.com/static/"
 # Ref: https://learn.microsoft.com/en-us/azure/service-connector/how-to-integrate-postgres?tabs=dotnet
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
